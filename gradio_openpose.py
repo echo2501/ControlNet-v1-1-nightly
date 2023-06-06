ae8c0387db583081825dca1ae74bddf765e352a9
echo2501 <133180307+echo2501@users.noreply.github.com>
2023-06-05 21:41:23 -0500

    Update gradio_openpose.py

--- gradio_openpose.py
+++ gradio_openpose.py
@@ -18,9 +18,9 @@ from cldm.ddim_hacked import DDIMSampler
 preprocessor = None
 
 model_name = 'control_v11p_sd15_openpose'
-model = create_model(f'./models/{model_name}.yaml').cpu()
-model.load_state_dict(load_state_dict('./models/v1-5-pruned.ckpt', location='cuda'), strict=False)
-model.load_state_dict(load_state_dict(f'./models/{model_name}.pth', location='cuda'), strict=False)
+model = create_model(f'./content/{model_name}.yaml').cpu()
+model.load_state_dict(load_state_dict('./content/test.safetensors', location='cuda'), strict=False)
+model.load_state_dict(load_state_dict(f'./content/{model_name}.pth', location='cuda'), strict=False)
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
